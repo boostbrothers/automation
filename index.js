a0408ef08524199ac0101fb05fa78082c35c56c6
ClaudeSeo <ehdaudtj@gmail.com>
2020-06-16 14:11:40 +0900

    init

---
+++ index.js
@@ -0,0 +1,110 @@
+const core = require('@actions/core');
+const { context, getOctokit } = require('@actions/github');
+
+const token = core.getInput('GITHUB_TOKEN', { required: true });
+const botToken = core.getInput('BOT_TOKEN');
+const masterBranch = core.getInput('MASTER_BRANCH');
+const developBranch = core.getInput('DEVELOP_BRANCH');
+const releaseBranch = core.getInput('RELEASE_BRANCH_PREFIX');
+const hotfixBranch = core.getInput('HOTFIX_BRANCH_PREFIX');
+const githubBot = getOctokit(token);
+const automationBot = getOctokit(botToken);
+const { owner, repo } = context.repo;
+
+const getTarget = (branch) => {
+  if (branch.startsWith(releaseBranch)) {
+    return developBranch;
+  }
+
+  if (branch.startsWith(hotfixBranch)) {
+    return developBranch;
+  }
+
+  return null;
+};
+
+const getVersion = (branch) => {
+  return branch.substring(branch.indexOf('/') + 1);
+};
+
+const createPullRequest = async (head, base, version) => {
+  const resp = await githubBot.pulls.create({
+    owner,
+    repo,
+    head,
+    base,
+    title: `chore(release): ${version}`,
+  });
+
+  const responseMessage = [
+    `head: ${head}, base: ${base}`,
+    'Create Pull Reuqest:',
+    `\tStatus Code: ${resp.status}`,
+    `\tPull Request Number: ${resp.data.number}`,
+  ].join('\n');
+
+  console.log(responseMessage);
+  return resp;
+};
+
+const approvePullRequest = async (prNumber) => {
+  const resp = await automationBot.pulls.createReview({
+    owner,
+    repo,
+    pull_number: prNumber,
+    event: 'APPROVE',
+  });
+
+  return resp;
+};
+
+const createTag = async (commitSha, version) => {
+  await automationBot.git.createTag({
+    owner,
+    repo,
+    tag: version,
+    message: version,
+    object: commitSha,
+    type: 'commit',
+  });
+
+  await automationBot.repos.createRelease({
+    owner,
+    repo,
+    tag_name: version,
+    name: version,
+  });
+};
+
+const run = async () => {
+  core.debug(JSON.stringify(context.payload));
+
+  if (!context.payload.pull_request.merged) {
+    core.info('pull request is not merged. Skpping...');
+    return;
+  }
+
+  const head = context.payload.pull_request.head.ref;
+  const base = context.payload.pull_request.base.ref;
+  const target = getTarget(head);
+  if (!target || base !== masterBranch) {
+    core.info('This PR base branch do not have prefix');
+    return;
+  }
+
+  const version = getVersion(head);
+
+  const pr = await createPullRequest(head, target, version);
+  await approvePullRequest(pr.data.number);
+
+  const mergeCommitSha = context.payload.pull_request.merge_commit_sha;
+  await createTag(mergeCommitSha, version);
+
+  core.debug(JSON.stringify(context.payload));
+};
+
+try {
+  run()
+} catch (error) {
+  core.setFailed(error.message);
+}
